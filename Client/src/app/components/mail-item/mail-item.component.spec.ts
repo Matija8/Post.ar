848521c0bf145e66930eb87670b18e80df8f3ecb
matija8 <matijanme@gmail.com>
2020-03-16 15:26:56 +0100

    Removed some tests made on ng new.

--- Client/src/app/components/mail-item/mail-item.component.spec.ts
+++ Client/src/app/components/mail-item/mail-item.component.spec.ts
@@ -20,6 +20,6 @@ describe('MailItemComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // TODO: toBeTruthy?
   });
 });
