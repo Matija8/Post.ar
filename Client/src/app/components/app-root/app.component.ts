bd1bef01beaee192579799933509a6afdaa9d475
matija8 <matijanme@gmail.com>
2020-04-21 22:00:08 +0200

    Removed app-root css and html.

--- Client/src/app/components/app-root/app.component.ts
+++ Client/src/app/components/app-root/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'postar-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <postar-header></postar-header>
+    <postar-main></postar-main>`
 })
 export class AppComponent {}
