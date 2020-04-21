f374ee91118994b39795a26d2dc527cd6f438742
matija8 <matijanme@gmail.com>
2020-04-21 13:54:36 +0200

    Refactored header and added letter to showMenuBtn.

--- Client/src/app/components/ui/header/header.component.ts
+++ Client/src/app/components/ui/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChangeThemeService } from '../../../services/ui-services/change-theme.service';
 import { AuthService } from '../../../services/mail-services/auth.service';
@@ -10,34 +10,28 @@ import { Subscription } from 'rxjs';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
-  @ViewChild('showMenu') showMenu: ElementRef;
-  @ViewChild('menu') menu: ElementRef;
-  userData: User = null;
+export class HeaderComponent implements OnInit, OnDestroy {
   authSubscription: Subscription = null;
-  menuVisible = false;
+  showMenuBtnVisibile: 'visible'|'hidden' = this.auth.loggedIn() ? 'visible' : 'hidden';
+  dropdownHidden = true;
+  letter = ' ';
 
   constructor(private changeThemeService: ChangeThemeService, private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.authSubscription = this.auth.currentUserData
-    .subscribe(data => {
-      this.userData = data;
-
-      this.menuVisible = false;
-      if (this.menu && this.menu.nativeElement) {
-        this.menu.nativeElement.style.visibility = 'hidden';
-      }
-      if (this.showMenu && this.showMenu.nativeElement) {
-        this.showMenu.nativeElement.style.visibility = this.auth.loggedIn() ? 'visible' : 'hidden';
+    .subscribe((userData: User) => {
+      this.showMenuBtnVisibile = !!userData ? 'visible' : 'hidden';
+      this.dropdownHidden = true;
+      if (userData) {
+        const name: string = userData.name;
+        if (typeof(name) === 'string' && name.length > 0) {
+          this.letter = name[0].toUpperCase();
+        }
+      } else {
+        this.letter = ' ';
       }
     });
-  }
-
-  ngAfterViewInit(): void {
-    if (this.showMenu && this.showMenu.nativeElement) {
-      this.showMenu.nativeElement.style.visibility = this.auth.loggedIn() ? 'visible' : 'hidden';
-    }
   }
 
   ngOnDestroy(): void {
@@ -49,16 +43,6 @@ export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
 
   toggleTheme(): void {
     this.changeThemeService.toggleDarkMode();
-  }
-
-  showMenuClick() {
-    const showMenu = this.showMenu.nativeElement as HTMLElement;
-    const menu = this.menu.nativeElement as HTMLElement;
-    if (!menu || !showMenu) {
-      return;
-    }
-    menu.style.visibility = this.menuVisible ? 'hidden' : 'visible';
-    this.menuVisible = !this.menuVisible;
   }
 
   logOut() {
