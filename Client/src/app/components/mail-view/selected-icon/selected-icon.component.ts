a87c948d8af2bbabcf2fd94fcae9d6628b0ad7fb
Matija8 <matijanme@gmail.com>
2020-06-29 16:06:44 +0200

    Selected-icon: prebaceno na checkbox.

--- Client/src/app/components/mail-view/selected-icon/selected-icon.component.ts
+++ Client/src/app/components/mail-view/selected-icon/selected-icon.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'postar-selected-icon',
-  templateUrl: './selected-icon.component.html'
+  templateUrl: './selected-icon.component.html',
+  styles: [':host { padding-top: 3px; margin-left: 5px } ']
 })
 export class SelectedIconComponent {
   @Output() toggle = new EventEmitter<void>();
